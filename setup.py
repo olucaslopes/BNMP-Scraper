53c7e712b814afd7e35f3ccd065bbc2a34894f36
Lucas Lopes <lucaslopesamorim@gmail.com>
2022-06-20 17:58:33 -0300

    Fix installation error

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup
 from pathlib import Path
-from bnmp_scraper.settings import __version__
+import re
+
+version_regex = r'__version__ = ["\']([^"\']*)["\']'
+with open("bnmp_scraper/settings.py", "r") as f:
+    text = f.read()
+    match = re.search(version_regex, text)
+
+    if match:
+        VERSION = match.group(1)
+    else:
+        raise RuntimeError("No version number found!")
 
 
 setup(
     name='bnmp_scraper',
-    version=__version__,
+    version=VERSION,
 
     url='https://github.com/olucaslopes/BNMP-Scraper',
     author='Lucas Lopes',
